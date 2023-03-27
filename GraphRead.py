84b51884f8d56313a6ae13d4b4d93fbc73d995c5
rkohub <rkoffstein@gmail.com>
2023-03-27 12:12:46 -0400

    Created Working Export
    Created Working Export

--- GraphRead.py
+++ GraphRead.py
@@ -1,21 +1,62 @@
+from Point import Point, Vector
+
 #Graph File Parse
 #2 Files
 #Start with the simple one
 
-f = open("graphData.txt", "r")
-f = str(f.read())
+f = open("L1Nodes.csv", "r")
+fstr = str(f.read())
 # print(type(f))
-linesArray = f.split("\n")
-print(linesArray)
+linesArray = fstr.split("\n")
+# print(linesArray)
 
-vertexNames = []
-doneVertex = False
 
-edges = []
-vertexCount = 0
+# xCords = []
+# yCords = []
+
+names = []
+
+# xMinMinus = 1500
+# yMinMinus = 750
+
 
 for i in range(0,len(linesArray)):
 	line = linesArray[i]
+	# print(line)
+	lineArr = line.split(",")
+	print(lineArr)
+	if(len(lineArr) > 1):
+		names.append(lineArr[0])
+
+f.close()
+
+f = open("L1Edges.csv", "r")
+fstr = str(f.read())
+# print(type(f))
+linesArray = fstr.split("\n")
+
+
+for i in range(0,len(linesArray)):
+	line = linesArray[i]
+	# print(line)
+	lineArr = line.split(",")
+	print(lineArr)
+	if(len(lineArr) > 1):
+		# names.append(lineArr[0])
+		pass
+
+
+		'''
+		if(i == 0):
+			labels = lineArr
+		else:
+			xCords.append(int(lineArr[1]))#-xMinMinus)
+			yCords.append(int(lineArr[2]))#-yMinMinus)
+			# positions.append(Point(int(lineArr[1])-xMinMinus,int(lineArr[2])-yMinMinus))
+			positions.append(Point(int(lineArr[1]),int(lineArr[2])))
+		#'''
+
+	'''
 	if(line == ''):
 		if(doneVertex):
 			break
@@ -33,5 +74,20 @@ for i in range(0,len(linesArray)):
 		v1Ind = vertexNames.index(v1)
 		v2Ind = vertexNames.index(v2)
 		edges[v1Ind][v2Ind] += 1
+	#'''
 
-print(edges)
+# print(edges)
+
+'''
+# print(xCords[2],yCords[2])
+minX = min(xCords)
+maxX = max(xCords)
+minY = min(yCords)
+maxY = max(yCords)
+
+print(positions[2])
+# print(len(positions))
+
+print(minX, maxX, maxX-minX)#1133, Min = 1637, 1500,(1500), 3000
+print(minY, maxY, maxY-minY)#1461, Min = 799, 750, (1750), 2500
+#'''
